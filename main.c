f2fef56443101d3fdf762d56e17187b9f64e0095
netoe1 <elynetobr@outlook.com>
2024-07-10 20:23:14 -0300

    creating dinamic allocation

--- main.c
+++ main.c
@@ -1,49 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 #include <string.h>
 #include <math.h>
+#include <signal.h>
 
-/*
-    The main diagonal of an Am×n matrix is the collection of aij elements
-    of the array where i=j.
-    The secondary diagonal of this matrix is formed by the elements aij where i+j=n+1.
-
-    PS:
-    n = matrix size's;
-    i = line
-    j = column
-*/
-#define SQ_2 2
-#define SQ_3 3
-
-float calc_det(float **m, const int __TYPEOFMATRIX)
+typedef struct matrixfloat
 {
+    float **p;
+    int i;
+    int j;
+} MatrixFloat;
 
-    float det_res;
-    float dp, ds;
+MatrixFloat createMatrix(const int i, const int j)
+{
+    // Creating matrix
 
-    dp = ds = det_res = 0;
-    switch (__TYPEOFMATRIX)
+    MatrixFloat newMatrix;
+
+    // Creating lines.
+    newMatrix.p = (float **)malloc(sizeof(float) * i);
+    // Creating columns
+    for (int a1 = 0; a1 < j; a1++)
     {
-    case SQ_2:
-        puts("CALC_DET:You choose 2-square matrix.");
-        det_res = (m[0][0] * m[1][1]) - (m[1][0] * m[0][1]);
-        break;
-    case SQ_3:
-        puts("CALC_DET:You choose 3-square matrix.");
-        break;
-
-    default:
-        puts("CALC_DET ERROR:You choose an invalid matrix type's, read the documentation file.");
-        break;
+        newMatrix.p[a1] = (float *)malloc(sizeof(float) * j);
     }
 
-    return det_res;
+    return newMatrix;
 }
-
 int main(void)
 {
-    printf("\nHello World!");
     return 0;
 }
